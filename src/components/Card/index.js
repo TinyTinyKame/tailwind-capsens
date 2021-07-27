73526fc57f13a3434dcd7bb4eb98da3a727091fe
Thierry THONGKHAM <thierry@capsens.eu>
2021-07-27 11:59:59 +0200

    Add styling

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,16 +1,16 @@
 function Card({ item }) {
   return (
-    <div>
+    <div className="flex items-center p-4 bg-white rounded-lg shadow-xs">
       <div>Nº{item.id}</div>
-      <img src={item.roundImg} alt={`${item.name} icon`}/>
-      <div>
-        <h3>{item.name}</h3>
-        <div>
+      <img className="mr-4 rounded-full" src={item.roundImg} alt={`${item.name} icon`}/>
+      <div className="flex flex-col">
+        <h3 className="text-lg font-semibold text-gray-700">{item.name}</h3>
+        <div className="text-sm font-medium text-gray-600">{item.species}</div>
+        <div className="flex flex-row items-center">
           {
-            item.types.map(type => (<div>{type}</div>))
+            item.types.map(type => (<div key={`${item.name}-${type}`} className="p-1 mr-4">{type}</div>))
           }
         </div>
-        <div>{item.species}</div>
       </div>
     </div>
   )
--- src/components/CardsList/index.js
+++ src/components/CardsList/index.js
@@ -2,14 +2,14 @@ import Card from '../Card'
 
 function CardsList({ list = [], title = 'Title' }) {
   return (
-    <div>
-      <h2>
+    <div className="container px-6 mx-auto">
+      <h2 className="my-6 text-2xl font-semibold text-gray-700">
         {title}
       </h2>
 
-      <div>
+      <div className="grid gap-6 mb-8 md:grid-cols-2 xl:grid-cols-4">
         {
-          list.map(item => (<Card item={item} />))
+          list.map(item => (<Card key={item.id} item={item} />))
         }
       </div>
     </div>
